183adec47f53177e1a9e80af399947330eba6fc5
iamarbazkhan <arbaz@leewayhertz.com>
2023-10-11 19:14:38 +0530

    sddsd

--- src/HeaderComponet.js
+++ src/HeaderComponet.js
@@ -4,16 +4,16 @@ function HeaderComponet(props) {
   return (
     <div className={styles.header}>
       <div>
-        <img className={styles.logoImg} src={"./images/logo.png"} />
+        <img className={styles.logoImg} src={"/images/logo.png"} />
       </div>
       <div className={styles.headerIcons}>
         <p className={styles.shopText}>Shop</p>
-        <img className={styles.headerLogo} src={"./images/hamburger-icon.svg"} />
+        <img className={styles.headerLogo} src={"/images/hamburger-icon.svg"} />
         <div className={styles.cartIconDiv} onClick={()=>{props.setShowCart(true)}}>
-          <img className={styles.headerLogo} src={"./images/shopping-bag.png"} />
+          <img className={styles.headerLogo} src={"/images/shopping-bag.png"} />
           <p className={styles.cartCount}>{props?.cartCount}</p>
         </div>
-        <img className={styles.headerLogo} src={"./images/hamburger-icon.svg"} />
+        <img className={styles.headerLogo} src={"/images/hamburger-icon.svg"} />
       </div>
     </div>
   );
